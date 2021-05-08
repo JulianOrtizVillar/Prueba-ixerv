a75db7eb77c59febc5ba63574fd45a2a745f9f08
JulianOrtizVillar <69093683+JulianOrtizVillar@users.noreply.github.com>
2021-05-08 00:14:58 -0500

    Login con c# y sql server
    Login con c# y sql server

---
+++ LoginJulianOrtizV/LoginJulianOrtizV/Controllers/UsuariosController.cs
@@ -0,0 +1,101 @@
+﻿using LoginJulianOrtizV.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginJulianOrtizV.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Uusarios
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UsuariosModel model)
+        {
+            String conectionString = "Data Source=localhost;" +
+                                        "Initial catalog=LogginJulian;" +
+                                        "User id=sa;" +
+                                        "Password=M@ria0313";
+
+            string query = "select * from tbl_login where username='" + model.UserName + "' and password='" + model.Password + "'";
+            using (SqlConnection conexion = new SqlConnection(conectionString))
+            {
+                SqlCommand consulta = new SqlCommand(query, conexion);
+                try
+                {
+                    conexion.Open();
+                    SqlDataReader Veryfy = consulta.ExecuteReader();
+                    if (Veryfy.Read())
+                    {
+                        UsuariosModel User = new UsuariosModel();
+                        User.UserName = Veryfy[0].ToString();
+                        
+                        conexion.Close();
+                        return View("Hellouser",User);
+                    }
+
+                    else
+                    {
+                        conexion.Close();
+                        return View("error");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+           
+        }
+
+        public ActionResult NewUser() 
+        {
+
+
+            return View();
+        
+        }
+
+        [HttpPost]
+        public ActionResult NewUser(UsuariosModel model)
+        {
+            String conectionString = "Data Source=localhost;" +
+                                        "Initial catalog=LogginJulian;" +
+                                        "User id=sa;" +
+                                        "Password=M@ria0313";
+            string query = "INSERT INTO [tbl_login] ([userName], [password], [email]) VALUES (" + "'" + model.UserName + "'" + "," + "'" + model.Password +"'"+","+ "'" + model.Email + "'" + ")";
+            using (SqlConnection conexion = new SqlConnection(conectionString))
+            {
+                SqlCommand consulta = new SqlCommand(query, conexion);
+                try
+                {
+                    conexion.Open();
+                    consulta.ExecuteReader();
+                    conexion.Dispose();
+                    return Redirect("~/usuarios/");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+        }
+
+        public ActionResult Contacto()
+        {
+            return View();
+        
+        }
+
+
+    }
+}
---
+++ LoginJulianOrtizV/LoginJulianOrtizV/Models/UsuariosModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoginJulianOrtizV.Models
+{
+    public class UsuariosModel
+    {
+        
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Nombre usuario")]
+        public string UserName { get; set; }
+
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+
+
+    }
+}
